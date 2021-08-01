8fd07db6072ec3d1e3f5d2c8ba026cdd4b523904
argyi <argyi.liaiyi@gmail.com>
2021-08-01 14:16:17 +0000

    Solve conflicts when merging.

--- Concurrency/src/day8/readwrite/RWLockLive.java
+++ Concurrency/src/day8/readwrite/RWLockLive.java
@@ -1,20 +1,6 @@
 package day8.readwrite;
 
 public final class RWLockLive implements ReadWriteLock {
-<<<<<<< HEAD
-	// private int readingReaders = 0; 
-	private int writingWriters = 0; 
-	// private int waitingWriters = 0;
-	private boolean isReadersTurn = false;
-
-	public synchronized void acquireRead() throws InterruptedException {
-		while (writingWriters > 0 && isReadersTurn == true) {
-			wait();
-		}
-	}
-
-	public synchronized void releaseRead() {
-=======
 	private int readingReaders = 0; 
 	private int writingWriters = 0; 
 	private int waitingWriters = 0;
@@ -30,23 +16,10 @@ public final class RWLockLive implements ReadWriteLock {
 	public synchronized void releaseRead() {
 		readingReaders--;   
 		isReadersTurn = false;
->>>>>>> upstream/master
 		notifyAll();
 	}
 
 	public synchronized void acquireWrite() throws InterruptedException {
-<<<<<<< HEAD
-		isReadersTurn = false;		
-		while (writingWriters > 0) {
-			wait();
-		}
-		writingWriters++; 
-		isReadersTurn = true;
-	}
-
-	public synchronized void releaseWrite() {
-		writingWriters--;                      
-=======
 		waitingWriters++;
 		while (readingReaders > 0 || writingWriters > 0) {
 			wait();
@@ -58,7 +31,6 @@ public final class RWLockLive implements ReadWriteLock {
 	public synchronized void releaseWrite() {
 		writingWriters--;   
 		isReadersTurn = true;
->>>>>>> upstream/master
 		notifyAll();
 	}
 }
